81f41c8f3a47e3f59bbd219da7c2fe18cb9783cf
hunzsig <mzyhaohaoren@qq.com>
2019-05-19 23:27:02 +0800

    up

--- Container/Interfaces.php
+++ Container/Interfaces.php
@@ -7,7 +7,6 @@ class Interfaces
     const default = [
         \PhpureCore\Interfaces\Bootstrap::class => \PhpureCore\Bootstrap\Bootstrap::class,
         \PhpureCore\Interfaces\Request::class => \PhpureCore\IO\Request::class,
-        \PhpureCore\Interfaces\IO::class => \PhpureCore\IO\IO::class,
     ];
 
     public static function get(): array
