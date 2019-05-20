132d1a002c6282afc913ea3cd85dbbee138ef4c0
马志洋 <mazhiyang@shein.com>
2019-05-20 15:23:27 +0800

    elastic

---
+++ Elasticsearch.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace PhpureCore;
+
+use Elasticsearch\ClientBuilder;
+
+class Elasticsearch
+{
+
+    public function __construct()
+    {
+        $elasticsearch_hosts = [
+            [
+                'host' => '127.0.0.1',
+                'port' => '9200',
+                'scheme' => 'http',
+            ],
+            /*
+            [
+                'host' => 'foo.com',
+                'port' => '9200',
+                'scheme' => 'https',
+                'user' => 'username',
+                'pass' => 'password!#$?*abc'
+            ],
+            */
+        ];
+        $client = ClientBuilder::create()
+            ->setHosts($elasticsearch_hosts)
+            ->setRetries(10)
+            ->build();
+        $params = [
+            'index' => 'my_index',
+            'type' => 'my_type',
+            'id' => 'my_id',
+            'body' => ['testField' => 'abc']
+        ];
+        $response = $client->index($params);
+        dump($response);
+    }
+
+}
--- IO.php
+++ IO.php
@@ -5,22 +5,11 @@
 
 namespace PhpureCore;
 
-use Elasticsearch\ClientBuilder;
-
 class IO
 {
 
     public static function response(object $request)
     {
-        $client = ClientBuilder::create()->build();
-        $params = [
-            'index' => 'my_index',
-            'type' => 'my_type',
-            'id' => 'my_id',
-            'body' => ['testField' => 'abc']
-        ];
-        $response = $client->index($params);
-        dump($response);
         //
         dump($request);
     }
