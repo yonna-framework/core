cea428c770922e849b698da2ed46e86b4d04627f
hunzsig <mzyhaohaoren@qq.com>
2019-05-28 21:58:52 +0800

    up

--- Config/Arrow.php
+++ Config/Arrow.php
@@ -2,14 +2,50 @@
 
 namespace PhpureCore\Config;
 
+use Str;
+
 class Arrow
 {
 
     protected static $stack = array();
 
+    /**
+     * @return array
+     */
     public static function fetch(): array
     {
         return static::$stack;
+    }
+
+    /**
+     * @param $key
+     * @param null $default
+     * @return array|bool|false|string|null
+     */
+    public static function env($key, $default = null)
+    {
+        $value = getenv($key);
+        if ($value === false) {
+            return $default;
+        }
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return null;
+        }
+        if (strlen($value) > 1 && Str::startsWith($value, '"') && Str::endsWith($value, '"')) {
+            return substr($value, 1, -1);
+        }
+        return $value;
     }
 
 }
