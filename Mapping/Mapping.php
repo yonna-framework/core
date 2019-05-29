14a5a81181c5245353feb8a48cf9ab7fecdc44a9
马志洋 <mazhiyang@shein.com>
2019-05-29 16:49:02 +0800

    mapping

--- Mapping/Mapping.php
+++ Mapping/Mapping.php
@@ -2,7 +2,123 @@
 
 namespace PhpureCore\Mapping;
 
+use PhpureCore\Glue\Response;
+use ReflectionClass;
+use ReflectionException;
+
 abstract class Mapping
 {
+
+    protected static $map_data = [];
+    protected static $fetch_cache = [];
+
+    /**
+     * 获取反射类名
+     * @return string
+     */
+    private static function getClassName()
+    {
+        return get_called_class() ?? __CLASS__;
+    }
+
+    /**
+     * 设置一个值的自定义参数
+     * @param $value
+     * @param $optKey
+     * @param $optVal
+     */
+    private static function setOptions($value, $optKey, $optVal)
+    {
+        $className = static::getClassName();
+        if (!isset(static::$map_data[$className])) {
+            static::$map_data[$className] = [];
+        }
+        static::$map_data[$className][$value][$optKey] = $optVal;
+    }
+
+    /**
+     * 获取一个值的自定义参数
+     * @param $value
+     * @param $optKey
+     * @return mixed
+     */
+    private static function getOption($value, $optKey)
+    {
+        return static::$map_data[static::getClassName()][$value][$optKey] ?? null;
+    }
+
+    /**
+     * 设置一个值的label
+     * @param $value
+     * @param $label
+     */
+    protected static function setLabel($value, $label)
+    {
+        static::setOptions($value, 'label', $label);
+    }
+
+    /**
+     * 获取一个值的label
+     * @param $value
+     * @return string
+     */
+    public static function getLabel($value)
+    {
+        return static::getOption($value, 'label') ?: '';
+    }
+
+    /**
+     * 设置一个值的status
+     * @param $value
+     * @param $status
+     */
+    protected static function setStatus($value, $status)
+    {
+        static::setOptions($value, 'status', $status);
+    }
+
+    /**
+     * 设置一个值的status
+     * @param $value
+     * @return string
+     */
+    public static function getStatus($value)
+    {
+        return static::getOption($value, 'status') ?: '1';
+    }
+
+    /**
+     * 反射mapping的数据
+     * @return mixed
+     */
+    protected static function fetch()
+    {
+        $class = static::getClassName();
+        if (!isset(static::$fetch_cache[$class])) {
+            try {
+                $objClass = new ReflectionClass($class);
+            } catch (ReflectionException $e) {
+                Response::exception($e->getMessage());
+            }
+            $arrConst = $objClass->getConstants();
+            static::$fetch_cache[$class] = $arrConst;
+        }
+        return static::$fetch_cache[$class] ?? [];
+    }
+
+    public static function toArray()
+    {
+        return static::fetch();
+    }
+
+    public static function toKV()
+    {
+        $data = static::fetch();
+        $kv = [];
+        foreach ($data as $v) {
+            $kv[$v] = static::getLabel($v);
+        }
+        return $kv;
+    }
 
 }
